4e8cde3e6da7dcdebe15c4cbf9fc1d118d4347f8
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-05-27 19:57:58 +0900

    add: github lang-mask

---
+++ src/github/sample.rs
@@ -0,0 +1,18 @@
+fn sanple(){
+    println!("Hello, world!");
+}
+fn sanple2(){
+    println!("Hello, world!");
+}
+fn sanple3(){
+    println!("Hello, world!");
+}
+fn sanple4(){
+    println!("Hello, world!");
+}
+fn sanple5(){
+    println!("Hello, world!");
+}
+fn sanple6(){
+    println!("Hello, world!");
+}
